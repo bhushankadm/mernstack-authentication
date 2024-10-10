835d27ee8d9bcccc142187e634d3cbf7e3778615
Rajnikant789 <rajnikantchikamal789@gmail.com>
2024-10-10 22:24:16 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    origin: 'https://extraordinary-seahorse-f64eb3.netlify.app', // Replace with your frontend's URL
     credentials: true
 }));
 
