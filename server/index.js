7508b7def1609fb7d82a2a7d96591d0c48929e5e
Rajnikant <129777933+rajnikant789@users.noreply.github.com>
2024-10-12 23:32:56 +0530

    remove dubal slah

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:3000', // Replace with your frontend's URL
+    origin:'http://localhost:3000', 
     credentials: true
 }));
 
